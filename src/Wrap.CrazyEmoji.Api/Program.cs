29258332dd450aad655d6886ae394b58300df251
Vejkam <kamienaitevejune@gmail.com>
2025-09-23 11:14:01 +0300

    (#3) Setup Mapster for mapping objects (#20)
    * (#3) Setup Mapster for mapping objects
    
    * Update src/Wrap.CrazyEmoji.Api/Program.cs

--- src/Wrap.CrazyEmoji.Api/Program.cs
+++ src/Wrap.CrazyEmoji.Api/Program.cs
@@ -11,9 +11,11 @@ try
         .CreateBuilder(args)
         .SetupObservability();
 
-    builder.Services.AddControllers();
     // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-    builder.Services.AddOpenApi();
+    builder.Services
+        .AddOpenApi()
+        .AddControllers()
+        .RegisterMapster();
 
     var app = builder.Build();
 
