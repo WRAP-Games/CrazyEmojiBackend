f232f8ee970f65fd02251ce298b07d142b01f4a5
Vejkam <kamienaitevejune@gmail.com>
2025-10-07 10:34:24 +0300

    Refactor

--- src/Wrap.CrazyEmoji.Api/Program.cs
+++ src/Wrap.CrazyEmoji.Api/Program.cs
@@ -1,7 +1,4 @@
 using System.Runtime.CompilerServices;
-using System.Text;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Serilog;
 using Wrap.CrazyEmoji.Api.Bootstraps;
 using Wrap.CrazyEmoji.Api.Extensions;
@@ -17,6 +14,7 @@ try
 
     // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
     builder.Services
+        .RegisterAuth(builder.Configuration)
         .AddExceptionHandler<GlobalExceptionHandler>()
         .AddProblemDetails()
         .AddOpenApi()
@@ -25,36 +23,11 @@ try
         .RegisterSignalR()
         .AddControllers();
 
-    var jwtSettings = builder.Configuration.GetSection("Jwt");
-    var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
-
-    builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = jwtSettings["Issuer"],
-            ValidAudience = jwtSettings["Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(key)
-        };
-    });
-
     var app = builder.Build();
 
     Log.Information("Application has been built for {EnvironmentName} environment.", builder.Environment.EnvironmentName);
 
     app.SetupWebApplication();
-
-    app.UseAuthentication();
-    app.UseAuthorization();
 
     app.Lifetime.ApplicationStarted.Register(() =>
     {
