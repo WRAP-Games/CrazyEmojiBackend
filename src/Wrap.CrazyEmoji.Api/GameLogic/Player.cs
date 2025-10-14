e9ad8a2ea57689dfb084ef809891dc968c791f7c
Vejkam <kamienaitevejune@gmail.com>
2025-10-14 12:30:23 +0300

    Refactor Player class to use Points struct for scoring and add game state properties

--- src/Wrap.CrazyEmoji.Api/GameLogic/Player.cs
+++ src/Wrap.CrazyEmoji.Api/GameLogic/Player.cs
@@ -4,6 +4,8 @@ public class Player
 {
     public string ConnectionId { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
-    public int Points { get; set; } = 0;
+    public Points Points { get; set; } = new Points(0);
+    public bool HasGuessed { get; set; } = false;
+    public bool GuessedRight { get; set; } = false;
     public PlayerRole Role { get; set; }
 }
