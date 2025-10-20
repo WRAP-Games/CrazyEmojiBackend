d1689445768c3787fdda6d49f97d96cda25a6206
Aiste Askinyte <aistexaskinyte@gmail.com>
2025-10-21 01:55:53 +0300

    updated usage of stream in program.cs

--- src/Wrap.CrazyEmoji.Api/Program.cs
+++ src/Wrap.CrazyEmoji.Api/Program.cs
@@ -3,7 +3,7 @@ using Serilog;
 using Wrap.CrazyEmoji.Api.Bootstraps;
 using Wrap.CrazyEmoji.Api.Extensions;
 using Wrap.CrazyEmoji.Api.Infrastructure;
-using Wrap.CrazyEmoji.Api.Abstractions;
+using Wrap.CrazyEmoji.Api.Services;
 
 [assembly: InternalsVisibleTo("Wrap.CrazyEmoji.UnitTests")]
 
@@ -31,9 +31,10 @@ try
 
     app.SetupWebApplication();
     
-    var wordService = app.Services.GetRequiredService<IWordService>();
+    var wordService = new WordService();
     using var stream = File.OpenRead("words.txt");
     await wordService.LoadWordsAsync(stream);
+    builder.Services.AddSingleton(wordService);
 
     app.Lifetime.ApplicationStarted.Register(() =>
     {
