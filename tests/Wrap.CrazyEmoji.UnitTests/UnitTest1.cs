cddab82f20cb80cd7e0e357bf275ace72c911c01
Aiste Askinyte <aiste.askinyte@mif.stud.vu.lt>
2025-11-11 12:17:47 +0200

    test1

--- tests/Wrap.CrazyEmoji.UnitTests/UnitTest1.cs
+++ tests/Wrap.CrazyEmoji.UnitTests/UnitTest1.cs
@@ -3,7 +3,13 @@
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Test1_Constructor_Throws_WhenNegative()
+    {
+        Assert.Throws<ArgumentException>(() => new Points(-1));
+    }
+
+    [Fact]
+    public void Test2_AdditionOperator_AddsCorrectly()
     {
         Assert.True(true);
     }
