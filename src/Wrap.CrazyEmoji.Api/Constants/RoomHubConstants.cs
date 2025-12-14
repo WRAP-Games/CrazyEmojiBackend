d34c49dc072713d3d546c1d523ba88e46aafb690
PatricijaBt <patricija.bt@gmail.com>
2025-12-14 17:27:04 +0200

    Updated constants and added command and error files to handle game logic errors more efficiently

--- src/Wrap.CrazyEmoji.Api/Constants/RoomHubConstants.cs
+++ src/Wrap.CrazyEmoji.Api/Constants/RoomHubConstants.cs
@@ -2,20 +2,21 @@
 
 public static class RoomHubConstants
 {
-    public const string UsernameSet = "UsernameSet";
-    public const string CreatedRoom = "CreatedRoom";
-    public const string JoinedRoom = "JoinedRoom";
-    public const string GameStarted = "GameStarted";
-    public const string ReceiveEmojis = "ReceiveEmojis";
-    public const string ReceiveWord = "ReceiveWord";
-
-    public const string CommanderSelected = "CommanderSelected";
-    public const string CommanderAnnounced = "CommanderAnnounced";
-    public const string CorrectGuess = "CorrectGuess";
-    public const string IncorrectGuess = "IncorrectGuess";
-    public const string PlayerLeft = "PlayerLeft";
+    public const string createdUser = "createdUser";
+    public const string userLoggedIn = "userLoggedIn";
+    public const string currentUserData = "currentUserData";
+    public const string userData = "userData";
+    public const string createdRoom = "createdRoom";
+    public const string joinedRoom = "joinedRoom";
+    public const string playerJoined = "playerJoined";
+    public const string playerLeft = "playerLeft";
+    public const string gameEnded = "gameEnded";
+    public const string gameStarted = "gameStarted";
+    public const string commanderSelected = "commanderSelected";
+    public const string recivedWord = "recivedWord";
+    public const string emojisRecieved = "emojisRecieved";
+    public const string wordChecked = "wordChecked";
+    public const string roundEnded = "roundEnded";
+    public const string roundStarted = "roundStarted";
     public const string Error = "Error";
-
-    public const string Username = "Username";
-    public const string RoomCode = "RoomCode";
 }
