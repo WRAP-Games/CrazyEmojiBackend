6f37fd304a8ff24c50f046eb8ed128a6d2a27ed0
Patricija Bietaitė <patricija.bt@gmail.com>
2025-12-14 17:29:57 +0200

    Updated constants and added command and error files to handle game logic errors more efficiently (#79)

---
+++ src/Wrap.CrazyEmoji.Api/Constants/RoomHubCommands.cs
@@ -0,0 +1,18 @@
+namespace Wrap.CrazyEmoji.Api.Constants;
+
+public static class RoomHubCommands
+{
+    public const string createUser = "createUser";
+    public const string loginUser = "loginUser";
+    public const string getCurrentUserData = "getCurrentUserData";
+    public const string getUserData = "getUserData";
+    public const string createRoom = "createRoom";
+    public const string joinRoom = "joinRoom";
+    public const string leftRoom = "leftRoom";
+    public const string startGame = "startGame";
+    public const string getCommander = "getCommander";
+    public const string getWord = "getWord";
+    public const string sendEmojis = "sendEmojis";
+    public const string checkWord = "checkWord";
+
+}
---
+++ src/Wrap.CrazyEmoji.Api/Constants/RoomHubErrors.cs
@@ -0,0 +1,19 @@
+namespace Wrap.CrazyEmoji.Api.Constants;
+
+public static class RoomHubErrors
+{
+    public const string incorrectUsername = "INCORRECT_USERNAME";
+    public const string incorrectPasword = "INCORRECT_PASSWORD";
+    public const string usernameTaken = "USERNAME_TAKEN";
+    public const string incorrectUsernamePassword = "INCORRECT_USERNAME_PASSWORD";
+    public const string incorrectConnectionId = "INCORRECT_CONNECTION_ID";
+    public const string forbiden = "FORBIDDEN";
+    public const string joinedDifferentRoom = "JOINED_DIFFERENT_ROOM";
+    public const string incorrectRoomName = "INCORRECT_ROOM_NAME";
+    public const string incorrectRoomCatergory = "INCORRECT_ROOM_CATEGORY";
+    public const string incorrectRoundAmount = "INCORRECT_ROUND_AMOUNT";
+    public const string incorrectRoundDuration = "INCORRECT_ROUND_DURATION";
+    public const string incorrectRoomCode = "INCORRECT_ROOM_CODE";
+    public const string roomGameStarted = "ROOM_GAME_STARTED";
+    public const string notEnoughPlayers = "NOT_ENOUGH_PLAYERS";
+}
