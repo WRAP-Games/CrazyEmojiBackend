4f93987d2a486841e8f420b5654ec56eaf6e9f7d
Patricija Bietaitė <patricija.bt@gmail.com>
2025-12-14 17:22:42 +0200

    Added database and entity files, as well as changed bootstrap file to connect to the database (#77)

---
+++ src/Wrap.CrazyEmoji.Api/Data/Entities/RoomMember.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Wrap.CrazyEmoji.Api.Data.Entities;
+
+[PrimaryKey("RoomCode", "Username")]
+public partial class RoomMember
+{
+    [Key]
+    [StringLength(6)]
+    public string RoomCode { get; set; } = null!;
+
+    [Key]
+    [StringLength(32)]
+    public string Username { get; set; } = null!;
+
+    [StringLength(9)]
+    public string Role { get; set; } = null!;
+
+    public long GameScore { get; set; }
+
+    [ForeignKey("RoomCode")]
+    [InverseProperty("RoomMembers")]
+    public virtual ActiveRoom RoomCodeNavigation { get; set; } = null!;
+
+    [ForeignKey("Username")]
+    [InverseProperty("RoomMembers")]
+    public virtual User UsernameNavigation { get; set; } = null!;
+}
