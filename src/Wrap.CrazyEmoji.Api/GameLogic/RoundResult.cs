b2d1aed3a968da3d1bcb8c0241915fe83f6965ca
Patricija Bietaitė <patricija.bt@gmail.com>
2025-12-22 23:04:05 +0200

    Updated word service and logic to handle word distribution better, track when rounds end and send results (#93)

---
+++ src/Wrap.CrazyEmoji.Api/GameLogic/RoundResult.cs
@@ -0,0 +1,7 @@
+public class RoundResult
+{
+    public string username { get; set; }
+    public bool guessedRight { get; set; }
+    public string guessedWord { get; set; }
+    public long gameScore { get; set; }
+}
