ebcd0bd201fdb15dfca657b62f9ceef965660b75
Rokas Braidokas <rokasbraidokas@gmail.com>
2025-12-23 11:35:19 +0200

    Added setup migration

---
+++ src/Wrap.CrazyEmoji.Api/Data/Migrations/20251223093455_Setup.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Wrap.CrazyEmoji.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Setup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:auth.aal_level", "aal1,aal2,aal3")
+                .Annotation("Npgsql:Enum:auth.code_challenge_method", "s256,plain")
+                .Annotation("Npgsql:Enum:auth.factor_status", "unverified,verified")
+                .Annotation("Npgsql:Enum:auth.factor_type", "totp,webauthn,phone")
+                .Annotation("Npgsql:Enum:auth.oauth_authorization_status", "pending,approved,denied,expired")
+                .Annotation("Npgsql:Enum:auth.oauth_client_type", "public,confidential")
+                .Annotation("Npgsql:Enum:auth.oauth_registration_type", "dynamic,manual")
+                .Annotation("Npgsql:Enum:auth.oauth_response_type", "code")
+                .Annotation("Npgsql:Enum:auth.one_time_token_type", "confirmation_token,reauthentication_token,recovery_token,email_change_token_new,email_change_token_current,phone_change_token")
+                .Annotation("Npgsql:Enum:realtime.action", "INSERT,UPDATE,DELETE,TRUNCATE,ERROR")
+                .Annotation("Npgsql:Enum:realtime.equality_op", "eq,neq,lt,lte,gt,gte,in")
+                .Annotation("Npgsql:Enum:storage.buckettype", "STANDARD,ANALYTICS,VECTOR")
+                .Annotation("Npgsql:PostgresExtension:extensions.pg_stat_statements", ",,")
+                .Annotation("Npgsql:PostgresExtension:extensions.pgcrypto", ",,")
+                .Annotation("Npgsql:PostgresExtension:extensions.uuid-ossp", ",,")
+                .Annotation("Npgsql:PostgresExtension:graphql.pg_graphql", ",,")
+                .Annotation("Npgsql:PostgresExtension:vault.supabase_vault", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Username = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    connectionId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("User_pkey", x => x.Username);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Text = table.Column<string>(type: "text", nullable: false),
+                    CategoryId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Words_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ActiveRooms",
+                columns: table => new
+                {
+                    RoomCode = table.Column<string>(type: "character varying(6)", maxLength: 6, nullable: false),
+                    RoomName = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    Rounds = table.Column<int>(type: "integer", nullable: false),
+                    RoundDuration = table.Column<int>(type: "integer", nullable: false),
+                    RoomCreator = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    CategoryId = table.Column<long>(type: "bigint", nullable: true),
+                    GameStarted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    RoundWord = table.Column<string>(type: "TEXT", nullable: true),
+                    EmojisSent = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    EmojisSentTime = table.Column<DateTime>(type: "timestamp", nullable: true),
+                    RoundEnded = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    CurrentRound = table.Column<int>(type: "integer", nullable: false, defaultValue: 0)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("ActiveRooms_pkey", x => x.RoomCode);
+                    table.ForeignKey(
+                        name: "FK_ActiveRooms_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FkUserCreator",
+                        column: x => x.RoomCreator,
+                        principalTable: "User",
+                        principalColumn: "Username",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomMembers",
+                columns: table => new
+                {
+                    RoomCode = table.Column<string>(type: "character varying(6)", maxLength: 6, nullable: false),
+                    Username = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    Role = table.Column<string>(type: "character varying(9)", maxLength: 9, nullable: false, defaultValueSql: "'Player'::character varying"),
+                    GameScore = table.Column<long>(type: "bigint", nullable: false),
+                    GuessedRight = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    GuessedWord = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("RoomMembers_pkey", x => new { x.RoomCode, x.Username });
+                    table.ForeignKey(
+                        name: "FkRoomMember",
+                        column: x => x.RoomCode,
+                        principalTable: "ActiveRooms",
+                        principalColumn: "RoomCode",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FkUserMember",
+                        column: x => x.Username,
+                        principalTable: "User",
+                        principalColumn: "Username",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActiveRooms_CategoryId",
+                table: "ActiveRooms",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActiveRooms_RoomCreator",
+                table: "ActiveRooms",
+                column: "RoomCreator");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Name",
+                table: "Categories",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomMembers_Username",
+                table: "RoomMembers",
+                column: "Username");
+
+            migrationBuilder.CreateIndex(
+                name: "User_connectionId_key",
+                table: "User",
+                column: "connectionId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Words_CategoryId",
+                table: "Words",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RoomMembers");
+
+            migrationBuilder.DropTable(
+                name: "Words");
+
+            migrationBuilder.DropTable(
+                name: "ActiveRooms");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
